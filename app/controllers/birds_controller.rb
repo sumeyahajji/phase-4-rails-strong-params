529c681903020eb2d74eddea43a866421e74be90
+ <+>
2023-03-24 14:41:21 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -7,8 +7,9 @@ class BirdsController < ApplicationController
   end
 
   # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+   # POST /birds
+   def create
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -21,5 +22,10 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  private
+  # all methods below here are private
 
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
